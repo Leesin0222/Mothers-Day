458655af24bb9a414ad7897c34db687f2aa69083
Leesin0222 <dragon02229@naver.com>
2022-05-08 23:47:23 +0900

    ♥️ :: 메인화면 클릭시 SelectActivity로 넘어가는 코드 작성

--- mompapa/app/src/main/java/com/company/mompapa/MainActivity.kt
+++ mompapa/app/src/main/java/com/company/mompapa/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.company.mompapa
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.company.mompapa.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.start.setOnClickListener {
+            val intent = Intent(this, SelectActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
